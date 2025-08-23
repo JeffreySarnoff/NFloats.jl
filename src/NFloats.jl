1ce3811dd459a0c1aa92aec88f4db0f45a165f35
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-08-23 00:17:36 -0400

    Implement ccall macro for C function calls
    Added a macro for C calls via function pointer.

--- src/NFloats.jl
+++ src/NFloats.jl
@@ -5,7 +5,7 @@ using FLINT_jll
 
 const libflint_filepath = FLINT_jll.libflint
 if !isfile(libflint_filepath)
-    throw(ErrorException("FLINT_jll not found")
+    throw(ErrorException("FLINT_jll not found"))
 end
     
 const libflint_handle = Libdl.dlopen(libflint_filepath)
@@ -16,6 +16,20 @@ macro libflint(function_name)
     return (:($function_name), libflint_handle)
 end
 
+# Quick macro for required C calls via function pointer
+macro ccall(fsym::Symbol, rett, argt, args...)
+    quote
+        local _fp = libfloat_fptr(fsym)
+        ccall(_fp, $(esc(rett)), $(esc(argt)), $(map(esc, args)...))
+    end
+end
+
+macro ccall(fname, rett, argt, args...)
+    quote
+        local _fp = libfloat_fptr(Symbol($fname))
+        ccall(_fp, $(esc(rett)), $(esc(argt)), $(map(esc, args)...))
+    end
+end
 
 
 end  # NFloats
