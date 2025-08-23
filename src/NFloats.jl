3ea591b52ad68cf16012d260f7ba2dc9142a64de
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-08-22 23:57:14 -0400

    Implement error handling and dynamic symbol lookup
    Added error handling for missing FLINT_jll and defined function pointer for dynamic symbol lookup.

--- src/NFloats.jl
+++ src/NFloats.jl
@@ -3,6 +3,15 @@ module NFloats
 using Libdl
 using FLINT_jll
 
+const libflint_filepath = FLINT_jll.libflint
+if !isfile(libflint_filepath)
+    throw(ErrorException("FLINT_jll not found")
+end
+    
+const libflint_handle = Libdl.dlopen(libflint_filepath)
+
+libfloat_fptr(sym) = dlsym(libflint_handle, sym)
+    
 macro libflint(function_name)
     return (:($function_name), FLINT_jll.libflint)
 end
