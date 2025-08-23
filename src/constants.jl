8d73be33159ae2908d64363403be9362f93aee81
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-08-23 01:50:28 -0400

    Add constants for GR_CTX_STRUCT_DATA_BYTES and NFLOAT_HEADER_LIMBS

--- src/constants.jl
+++ src/constants.jl
@@ -1,5 +1,8 @@
 const CPtr = Ptr{Nothing}
 
+const GR_CTX_STRUCT_DATA_BYTES = 6 * sizeof(UInt32)
+const NFLOAT_HEADER_LIMBS = 2
+
 # flags 
 # NFLOAT_ALLOW_UNDERFLOW does not slow vector processing
 # flags NFLOAT_ALLOW_INF slows vector processing
@@ -12,5 +15,3 @@ const NFLOAT_ALLOW_NAN       = 0x04
 const NFLOAT_ALLOW_ALL       = 0x04 | 0x02 | 0x01
 const NFLOAT_DISALLOW_NAN    = 0x02 | 0x01
 const NFLOAT_DISALLOW_INF    = 0x04 | 0x01
-
-const GR_CTX_STRUCT_DATA_BYTES = 6 * sizeof(UInt32)
