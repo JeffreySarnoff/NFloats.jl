d0a1f0162f80f4e89cc2bd30270daefd92c69e3c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-08-23 01:22:18 -0400

    Clean up NFLOAT flags in NFloats.jl
    Removed unused flags related to NFLOAT processing.

--- src/NFloats.jl
+++ src/NFloats.jl
@@ -7,23 +7,9 @@ const libflint_filepath = FLINT_jll.libflint
 if !isfile(libflint_filepath)
     throw(ErrorException("FLINT_jll not found"))
 end
-    
 const libflint_handle = Libdl.dlopen(libflint_filepath)
 
-const CPtr = Ptr{Nothing}
-
-# flags 
-# NFLOAT_ALLOW_UNDERFLOW does not slow vector processing
-# flags NFLOAT_ALLOW_INF slows vector processing
-# flags NFLOAT_ALLOW_NAN slows vector processing
-
-const NFLOAT_ALLOW_UNDERFLOW = 0x01
-const NFLOAT_ALLOW_INF       = 0x02
-const NFLOAT_ALLOW_NAN       = 0x04
-
-const NFLOAT_ALLOW_ALL       = 0x04 | 0x02 | 0x01
-const NFLOAT_DISALLOW_NAN    = 0x02 | 0x01
-const NFLOAT_DISALLOW_INF    = 0x04 | 0x01
+include("constants.jl")
 
 
 libfloat_fptr(sym::Symbol) = dlsym(libflint_handle, sym)
