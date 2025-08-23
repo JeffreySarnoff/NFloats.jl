1265512d9b3ce632b4a084af954bdf68210f0b06
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2025-08-23 02:02:51 -0400

    Refactor nfloat_struct and update related constants

--- src/structs.jl
+++ src/structs.jl
@@ -17,58 +17,51 @@ mutable struct nfloat_ctx_struct{Precision, Flags}
     end
 end
 
-const NFLOAT_HEADER_LIMBS = 2
-mutable struct nfloat_struct{P,F}
+mutable struct nfloat_struct{Precision, Flags}
     head::NTuple{NFLOAT_HEADER_LIMBS,UInt}
-    d::NTuple{P,UInt} # FIXME: Should be different for 32 bit systems
+    d::NTuple{Precision, UInt}
 
-    function nfloat_struct{P,F}() where {P,F}
-        @assert P isa Int && F isa Int
-        res = new{P,F}()
-        init!(res, nfloat_ctx_struct{P,F}())
+    function nfloat_struct{Precision::Int, Flags::Int}() where {Precision, Flags}
+        res = new{Precision, Flags}()
+        init!(res, nfloat_ctx_struct{Precision, Flags}())
         return res
     end
 end
 
-# or
-
-# gr_ctx_struct (public header). We mirror it by value.
-const GR_CTX_STRUCT_DATA_BYTES = 6*sizeof(Culong)
-
-struct GRDataBlob
-    bytes::NTuple{GR_CTX_STRUCT_DATA_BYTES, UInt8}
-end
-
-struct GrCtxStruct
-    data::GRDataBlob
-    which_ring::Culong
-    sizeof_elem::Clong
-    methods::Ptr{Cvoid}
-    size_limit::Culong
-end
-
-mutable struct GrCtx
-    ctx::GrCtxStruct
-end
-
-@inline _ctxref(g::GrCtx) = Ref(g.ctx)
-
-# nfloat{BITS}_struct: head[2] + d[bits/64] (public header pattern)
-# (If your FLINT build changes this, adjust below accordingly.)
-const FLINT_BITS = 64
-
-@generated function _nlimbs(::Val{B}) where {B}
-    n = Int(B) ÷ FLINT_BITS
-    :(Val{$n}())
-end
-  
+#???
 macro def_nfloat_struct(bits)
     nb = bits ÷ FLINT_BITS
-    T  = Symbol("NFloat", bits, "Struct")
+    T  = Symbol("NFLOAT", bits)
     quote
-        struct $T
+        struct $T <: Real
             head::NTuple{2, Culong}
             d::NTuple{$nb, Culong}
         end
     end
 end
+
+@eval begin
+    $(@def_nfloat_struct(64))
+    $(@def_nfloat_struct(128))
+    $(@def_nfloat_struct(192))
+    $(@def_nfloat_struct(256))
+    $(@def_nfloat_struct(384))
+    $(@def_nfloat_struct(512))
+    $(@def_nfloat_struct(768))
+    $(@def_nfloat_struct(1024))
+    $(@def_nfloat_struct(2048))
+    $(@def_nfloat_struct(4096))
+end
+
+# Shorthands
+const NFloat64   = NFLOAT64
+const NFloat128  = NFLOAT128
+const NFloat192  = NFLOAT192
+const NFloat256  = NFLOAT256
+const NFloat384  = NFLOAT384
+const NFloat512  = NFLOAT512
+const NFloat768  = NFLOAT768
+const NFloat1024 = NFLOAT1024
+const NFloat2048 = NFLOAT2048
+const NFloat4096 = NFLOAT4096
+
